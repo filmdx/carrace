fd0fae674d60297c1aefb46db8c65dab8bf805cd
film <film@LAPTOP-DK17KV56>
2024-10-25 23:53:49 +0700

    Add project files.

---
+++ carrace/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace carrace
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
